775190b47062cbff5e28885c238f4b92c4a9ce28
John <daras.giannhs@gmail.com>
2017-10-22 00:13:38 +0300

    Images added. Screens combined. Work goes on!

--- Otto/app/src/main/java/com/espa/ceos/otto/DonationActivity.java
+++ Otto/app/src/main/java/com/espa/ceos/otto/DonationActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
