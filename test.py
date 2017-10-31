9a7235de870fa0247c48c4403f38cbf977c00da7
papachristoumarios <papachristoumarios@gmail.com>
2017-10-31 20:19:48 +0200

    added new code for locker demos

--- test.py
+++ test.py
@@ -4,7 +4,44 @@ global firebase
 firebase = firebase.FirebaseApplication('https://shop-n-out.firebaseio.com', None)
 from glob import glob
 import serial
+import RPi.GPIO as GPIO
 
+global mode
+mode = 'arduino'
+
+class Locker:
+	
+	def __init__(self):
+		if mode == 'arduino':
+			self.ser = serial.Serial(glob('/dev/ttyACM*')[0])		
+		elif mode == 'pi':
+			GPIO.setmode(GPIO.BCM)
+			GPIO.setup(4, GPIO.OUT)
+			self.pwm = GPIO.PWM(4 , 50) # GPIO 4 @50Hz
+			self.pwm.start(5)
+			self.toangle = lambda x : float(x) / 10.0 + 2.5 # tune for servo
+		else:
+			raise Error('Unknown mode, please select arduino or pi')
+		self.lock()
+		
+	def lock(self):
+		if mode == 'arduino':
+			self.ser.write('120')
+		elif mode == 'pi':
+			self.pwm.ChangeDutyCycle(toangle(120))
+		
+	def unlock(self):
+		if mode == 'arduino':
+			self.ser.write('20')	
+		elif mode == 'pi':
+			self.pwm.ChangeDutyCycle(toangle(20))
+			
+	def __del__(self):
+		if mode == 'arduino':
+			self.ser.close()
+		elif mode == 'pi':
+			self.pwm.stop()
+			GPIO.cleanup()		
 
 def itemsKeys():
 	temp=firebase.get('/items',None)
@@ -32,15 +69,15 @@ def resetDb():
 def unlock():
 	return firebase.get('/unlock',None)['value'] == 1
 
-ser = serial.Serial(glob('/dev/ttyACM*')[0])
-ser.write('120')
+locker = Locker()
 
 while not unlock():
 	print 'locked'
 	
 print 'unlocked'
-ser.write('0')	
-ser.close()
+
+locker.unlock()	
+del locker
 
 resetDb()
 
